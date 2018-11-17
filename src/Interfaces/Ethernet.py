2bfcc46f93cb6937d5b950f091a88d85ee99f8eb
FATESAIKOU <qzsecftbhhhh@gmail.com>
2018-11-17 18:28:41 +0800

    Complete Ftp interface instance

--- src/Interfaces/Ethernet.py
+++ src/Interfaces/Ethernet.py
@@ -62,12 +62,42 @@ class Ftp( InterfaceBase ):
         self.__address__    = address
         self.__model_root__ = model_root
         self.__script_dir__ = script_dir
+        self.__profile__    = None
+
+    def __RenewProfile__( self ):
+        profile_str = subprocess.check_output([
+            self.__script_dir__ + '/ftp_test.sh',
+            self.__address__,
+            self.__model_root__,
+            self.__address__,
+            self.__model_root__,
+            '16384'
+        ])
+
+        self.__profile__ = json.loads(profile_str)
 
     def HasFile( self, model_name ):
-        pass
+        filelist_str = subprocess.check_output([
+            self.__script_dir__ + '/ftp_list.sh',
+            self.__address__,
+            self.__model_root__
+        ])
+
+        return model_name in json.loads(filelist_str).keys()
 
     def GetFile( self, model_name ):
-        pass
+        report_str = subprocess.check_output([
+            self.__script_dir__ + '/ftp_get.sh',
+            self.__address__,
+            self.__model_root__,
+            model_name,
+            self.__model_root__
+        ])
 
-    def GetProfile( self ):
-        pass
+        return json.loads(report_str)
+
+    def GetProfile( self, renew=False ):
+        if renew or self.__profile__ == None:
+            self.__RenewProfile__()
+
+        return self.__profile__
