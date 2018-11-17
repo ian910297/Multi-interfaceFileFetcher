a1dcbd0c8bebf5c7728486757ee0a8b9450587cf
FATESAIKOU <qzsecftbhhhh@gmail.com>
2018-11-17 18:04:09 +0800

    Fix bluetooth_get.sh script output format

--- scripts/bluetooth_get.sh
+++ scripts/bluetooth_get.sh
@@ -9,4 +9,4 @@ DEST=$5
 cd $DEST
 download_time=$( (time obexftp -b $ADDR -B $CHANNAL --chdir $TARGET_DIR --get $FILENAME) 2>&1 | grep real | awk -F'm' '{ print $2 }' | awk -F's' '{print $1}' );
 filesize=$( ls -l  $FILENAME | awk '{ print $5 }' )
-echo "{ \"$FILENAME\": $filesize }"
+echo "{ \"$FILENAME\": [$filesize, $download_time] }"
